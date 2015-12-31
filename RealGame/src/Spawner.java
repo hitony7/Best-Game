1cdc7be4d0218b6e17b888220093b6e364821819
Tony <Tony@BlackShinobi-PC>
2015-12-30 22:07:28 -0700

    test

--- RealGame/src/Spawner.java
+++ RealGame/src/Spawner.java
@@ -1,14 +1,19 @@
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Timer;
 
-public class Spawner {
+public class Spawner implements ActionListener {
 	public Timer spawninterval;
 	
 	public Spawner(){
-		 spawninterval = new Timer(1000 / 60, (ActionListener) this);
+		 spawninterval = new Timer(1000 / 60, this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
-		
-
 }
